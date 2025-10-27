8bf3b18aa275d74c758e2b387e4239fcd2714358
ajwamujahid <mujahidajwa1207@gmail.com>
2025-10-27 13:08:36 +0500

    react setup

---
+++ my-project/src/App.tsx
@@ -0,0 +1,13 @@
+
+ import './index.css'
+
+function App() {
+  return (
+    <div className="p-10 bg-blue-500 text-white rounded-2xl shadow-lg">
+    Hello Tailwind 
+  </div>
+  
+  )
+}
+
+export default App
