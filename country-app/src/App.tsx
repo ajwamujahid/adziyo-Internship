ad7072122c489a751b42df33c0cbf14c2f60aea4
ajwamujahid <mujahidajwa1207@gmail.com>
2025-10-31 12:52:33 +0500

    React package install

---
+++ country-app/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-700">
+        Welcome to the REST Country API APP
+      </h1>
+    </div>
+  );
+}
+
+export default App;
